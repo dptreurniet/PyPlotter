c66ed8240b87da5e3790467a612c7ab151f1086d
Daan Treureniet <dptreurniet@gmail.com>
2018-12-22 00:56:29 +0100

    Deleted all old files

--- pyplotter.py
+++
@@ -1,8 +0,0 @@
-# Created by Daan Treurniet
-
-from datastore import DataStore
-from datagenerator import sequence_intcanlog
-
-data_store = DataStore()
-
-data_store.load_file('data/test_data.mat')
